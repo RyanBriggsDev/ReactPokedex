cc53b23a8dc274011c2abd4c78c6a3d04af39acb
RyanMatthewBriggs <ryan.briggs248@gmail.com>
2022-07-18 15:56:22 +0100

    added loading component

--- src/components/layout/Loading.jsx
+++ src/components/layout/Loading.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import pokeball from '../../assets/pokeball.gif'
 
 function Loading() {
   return <div>
-    <img src='../assets/ball-pokemon.gif' alt="pokeball spinning" />
+    <img src={pokeball} alt='spinning pokeball' width="150px" height="150px"/>
   </div>
 }
 
